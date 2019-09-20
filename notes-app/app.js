ee337e443d9bc987e4eb3382ce05733f3cd3ef12
Jozeanne Belomo <jozeanne.belomo@gmail.com>
2019-09-20 18:51:03 -0300

    node-course: add text in green using chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,8 +3,12 @@ import * as validator from 'validator';
 import name from './utils.js';
 import getNotes from './notes.js'; 
 
+const chalk = require('chalk');
+
 console.log(name);
 console.log(getNotes());
 
 console.log(validator.isEmail('andrew@gmail.com'));
 console.log(validator.isURL('https://keep.google.com'));
+
+console.log(chalk.green('Success!!!'));
